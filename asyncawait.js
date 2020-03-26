3c63cbdccb66032ce179af38dbe1a7ca81b9c30a
mksuresh9 <54421724+mksuresh9@users.noreply.github.com>
2020-03-26 22:52:45 +0530

    Add files via upload

---
+++ asyncawait.js
@@ -0,0 +1,28 @@
+
+
+console.log('asyncfun1');
+console.log('asyncfun2');
+
+const asyncfun3 = async ()=>{
+    const loaduserdata= new Promise((resolve, reject)=>{
+        setTimeout(()=>resolve('loaded'), 2000)
+
+    })
+
+    const verifieddata = new Promise((resolve, reject)=>{
+        setTimeout(()=>resolve('verified data'), 3000);
+    });
+
+    let loaded  = await loaduserdata;
+    console.log("loaduserdata");
+    console.log("loaded");
+
+    let verified=  await verifieddata;
+    
+    return verifieddata;
+}
+
+asyncfun3().then((i)=>console.log(i));
+
+console.log('asyncfun4');
+console.log('asyncfun5');
